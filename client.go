6c6178aaad10e03696c1f852d6cd5b3591c5c757
Tom Young <tom@tomcat-engineering.com>
2020-04-30 10:18:02 +0100

    Update public API to match latest official client

--- client.go
+++ client.go
@@ -5,15 +5,15 @@ import (
 	"context"
 
 	"github.com/influxdata/influxdb-client-go"
+	"github.com/influxdata/influxdb-client-go/api"
+	"github.com/influxdata/influxdb-client-go/domain"
 )
-
 
 type InfluxDBRobustClient struct {
 	BaseClient influxdb2.InfluxDBClient
-	db *datastore
-	writeApis []influxdb2.WriteApi
+	db         *datastore
+	writeApis  []influxdb2.WriteApi
 }
-
 
 func NewClientWithOptions(serverUrl string, authToken string, bufferFile string, options *influxdb2.Options) (*InfluxDBRobustClient, error) {
 	db, err := NewDatastore(bufferFile)
@@ -22,7 +22,7 @@ func NewClientWithOptions(serverUrl string, authToken string, bufferFile string,
 	}
 	return &InfluxDBRobustClient{
 		BaseClient: influxdb2.NewClientWithOptions(serverUrl, authToken, options),
-		db: db,
+		db:         db,
 	}, nil
 }
 
@@ -47,6 +47,10 @@ func (c *InfluxDBRobustClient) Close() {
 	c.db.Close()
 }
 
+func (c *InfluxDBRobustClient) Setup(ctx context.Context, username, password, org, bucket string, retentionPeriodHours int) (*domain.OnboardingResponse, error) {
+	return c.BaseClient.Setup(ctx, username, password, org, bucket, retentionPeriodHours)
+}
+
 func (c *InfluxDBRobustClient) Ready(ctx context.Context) (bool, error) {
 	return c.BaseClient.Ready(ctx)
 }
@@ -55,9 +59,27 @@ func (c *InfluxDBRobustClient) QueryApi(org string) influxdb2.QueryApi {
 	return c.BaseClient.QueryApi(org)
 }
 
+func (c *InfluxDBRobustClient) AuthorizationsApi() api.AuthorizationsApi {
+	return c.BaseClient.AuthorizationsApi()
+}
+
+func (c *InfluxDBRobustClient) OrganizationsApi() api.OrganizationsApi {
+	return c.BaseClient.OrganizationsApi()
+}
+
+func (c *InfluxDBRobustClient) UsersApi() api.UsersApi {
+	return c.BaseClient.UsersApi()
+}
+
 // Rather than influxdb2.writeApiImpl, we return our own implementation
 func (c *InfluxDBRobustClient) WriteApi(org, bucket string) influxdb2.WriteApi {
 	w := NewWriter(c.BaseClient, c.db, org, bucket, c.BaseClient.Options())
 	c.writeApis = append(c.writeApis, w)
 	return w
+}
+
+// Allow direct access to the underlying blocking client - blocking writes will tell the caller
+// that the write failed, so we don't need the magic persistent buffer.
+func (c *InfluxDBRobustClient) WriteApiBlocking(org, bucket string) influxdb2.WriteApiBlocking {
+	return c.BaseClient.WriteApiBlocking(org, bucket)
 }
